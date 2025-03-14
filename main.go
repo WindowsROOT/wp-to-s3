1866346278e182f9efd7d21cdf6301511446391b
WindowsROOT <greanner@gmail.com>
2025-03-14 13:55:39 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,45 +1,51 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env file if it exists
+	godotenv.Load()
+
 	// Load AWS Credentials from .env
 	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
 	awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	awsRegion := os.Getenv("AWS_REGION")
 	bucketName := os.Getenv("AWS_S3_BUCKET")
-	uploadDir := os.Getenv("UPLOAD_DIR") // Load upload directory from .env
+	uploadDir := os.Getenv("UPLOAD_DIR")
 
 	if uploadDir == "" {
 		log.Fatal("UPLOAD_DIR is not set in .env")
 	}
 
-	// Load AWS SDK Config
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(awsRegion),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(awsAccessKey, awsSecretKey, "")),
-	)
+	// Create AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, ""),
+	})
 	if err != nil {
-		log.Fatalf("Failed to load AWS config: %v", err)
+		log.Fatalf("Failed to create AWS session: %v", err)
 	}
 
-	s3Client := s3.NewFromConfig(cfg)
+	// Create S3 client
+	s3Client := s3.New(sess)
 
 	// Walk through all files in UPLOAD_DIR
-	err = filepath.WalkDir(uploadDir, func(path string, d os.DirEntry, err error) error {
+	err = filepath.Walk(uploadDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !d.IsDir() { // Upload only files, skip directories
+
+		if !info.IsDir() { // Upload only files, skip directories
 			relativePath, _ := filepath.Rel(uploadDir, path)
 			err := uploadToS3(s3Client, bucketName, path, relativePath)
 			if err != nil {
@@ -56,16 +62,16 @@ func main() {
 	}
 }
 
-func uploadToS3(client *s3.Client, bucket, filePath, s3Key string) error {
+func uploadToS3(client *s3.S3, bucket, filePath, s3Key string) error {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: &bucket,
-		Key:    &s3Key,
+	_, err = client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(s3Key),
 		Body:   file,
 	})
 	return err
